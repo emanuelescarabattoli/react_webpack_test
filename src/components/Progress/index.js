01e756d3194241593aab89c50e43d7cfae0fb946
Emanuele Scarabattoli <emanuele.scarabattoli@evonove.it>
2019-07-25 17:18:15 +0200

    Some example

--- src/components/Progress/index.js
+++ src/components/Progress/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
+import style from "./style.scss";
 
 const styleContainer = {
   position: "fixed",
@@ -8,26 +9,49 @@ const styleContainer = {
   left: "0px",
   width: "100%",
   height: "9px",
-  overflow: "hidden"
+  overflow: "hidden",
 };
 
-const styleContent = {
+const styleBackground = {
   height: "4px",
 };
 
+const styleContent = {
+  height: "40px",
+  transition: "width 1s ease-in-out"
+};
+
 class ReactTopProgressBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: props.value
+    };
+  }
+
+  componentWillReceiveProps(props) {
+
+    requestAnimationFrame(() => this.setState({ value: props.value }));
+  }
+
   render() {
     return (
       <div style={{
         ...styleContainer,
-        backgroundColor: this.props.backgroundColor
+        display: this.state.value <= 0 || this.state.value >= 100 ? "none" : "block"
       }}>
         <div style={{
-          ...styleContent,
-          width: `${this.props.value}%`,
-          backgroundColor: this.props.color,
-          boxShadow: this.props.withShadow ? "0px 0px 5px 0px #00000055" : "none",
-        }} />
+          ...styleBackground,
+          backgroundColor: this.props.backgroundColor
+        }}>
+          <div className={this.props.striped ? style.striped : undefined} style={{
+            ...styleContent,
+            width: `${this.state.value}%`,
+            backgroundColor: this.props.color,
+            boxShadow: this.props.withShadow ? "0px 0px 5px 0px #00000055" : "none",
+          }} />
+        </div>
       </div>
     );
   }
@@ -37,14 +61,16 @@ ReactTopProgressBar.propTypes = {
   value: PropTypes.number,
   color: PropTypes.string,
   withShadow: PropTypes.bool,
-  backgroundColor: PropTypes.string
+  backgroundColor: PropTypes.string,
+  striped: PropTypes.bool
 };
 
 ReactTopProgressBar.defaultProps = {
   value: 0,
   color: "#CC0000",
   withShadow: false,
-  backgroundColor: "transparent"
+  backgroundColor: "transparent",
+  striped: false
 };
 
 export default ReactTopProgressBar;
--- src/containers/Demo/index.js
+++ src/containers/Demo/index.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
+import ReactTopProgressBar from "react-top-progress-bar";
+
 
 class Demo extends Component {
 
   render() {
     const url = process.env.URL;
     return (
-      <div>{url}</div>
+      <div>
+        <ReactTopProgressBar striped value={50} />
+        {url}
+      </div>
     );
   }
 }
